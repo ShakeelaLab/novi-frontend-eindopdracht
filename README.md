23a59225c2791acc43a97d1f8c2d05fd6d225211
ShakeelaLab <shakeela@live.nl>
2026-01-11 15:43:01 +0100

    aanpassingen aan theme gedaan en README file ingevuld

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Bookstore - React App
 
-
-
 ## Index
 
+1.0 Over Bookstore App
+1.1 Screenshot van de Homepagina
+1.2 Screenshot van de Favorietenpagina
+2.0 Installation and requirements
+2.1 Available Scripts
 
+**1.0. Over Bookstore App**
+
+In deze app kan je zoeken naar boeken, waarna je een selectie van boeken te zien krijgt met jaaruitgave, auteur, samenvatting en je kan je boek in je favorieten opslaan.
+
+**1.1 Screenshot van de Homepage**
+
+**1.2 Screenshot van de Favorietenpagina**
 
 ## Installeren
 
+`npm install`
+
+(http://localhost:5173)
--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,9 +1,159 @@
 import './Favorites.css';
+import Button from "../../components/button/Button.jsx";
 
 function Favorites() {
     return (
         <>
-            
+                <h1>Header 1</h1>
+                <hr/>
+                <h2>Header 2</h2>
+                <h3>Header 3</h3>
+                <h4>Header 4</h4>
+                <h5>Header 5</h5>
+            <Button
+            buttontext='button'
+            />
+                <p>hier komt tekst</p>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Culpa dolorum fugit ipsam
+                    laborum necessitatibus pariatur placeat
+                    reiciendis ullam. Architecto, harum,
+                    temporibus. Ab adipisci alias aliquid
+                    aperiam aspernatur cumque excepturi
+                    explicabo fugiat harum impedit inventore
+                    ipsa itaque labore natus, nostrum officiis
+                    quaerat quasi reprehenderit sed similique
+                    sit soluta suscipit temporibus. Accusamus
+                    accusantium ad asperiores aut commodi,
+                    debitis expedita id ipsam iste magnam nisi
+                    provident quae quam quibusdam rem velit
+                    voluptatem. Assumenda at dolorem eum minus
+                    odio porro qui sint, voluptatem. Delectus
+                    dignissimos dolorem dolores esse est,
+                    incidunt iusto laboriosam maxime mollitia
+                    necessitatibus officia quos ratione soluta
+                    tempore veritatis! Adipisci dolorum,
+                    odit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Culpa dolorum fugit ipsam
+                    laborum necessitatibus pariatur placeat
+                    reiciendis ullam. Architecto, harum,
+                    temporibus. Ab adipisci alias aliquid
+                    aperiam aspernatur cumque excepturi
+                    explicabo fugiat harum impedit inventore
+                    ipsa itaque labore natus, nostrum officiis
+                    quaerat quasi reprehenderit sed similique
+                    sit soluta suscipit temporibus. Accusamus
+                    accusantium ad asperiores aut commodi,
+                    debitis expedita id ipsam iste magnam nisi
+                    provident quae quam quibusdam rem velit
+                    voluptatem. Assumenda at dolorem eum minus
+                    odio porro qui sint, voluptatem. Delectus
+                    dignissimos dolorem dolores esse est,
+                    incidunt iusto laboriosam maxime mollitia
+                    necessitatibus officia quos ratione soluta
+                    tempore veritatis! Adipisci dolorum,
+                    odit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Culpa dolorum fugit ipsam
+                    laborum necessitatibus pariatur placeat
+                    reiciendis ullam. Architecto, harum,
+                    temporibus. Ab adipisci alias aliquid
+                    aperiam aspernatur cumque excepturi
+                    explicabo fugiat harum impedit inventore
+                    ipsa itaque labore natus, nostrum officiis
+                    quaerat quasi reprehenderit sed similique
+                    sit soluta suscipit temporibus. Accusamus
+                    accusantium ad asperiores aut commodi,
+                    debitis expedita id ipsam iste magnam nisi
+                    provident quae quam quibusdam rem velit
+                    voluptatem. Assumenda at dolorem eum minus
+                    odio porro qui sint, voluptatem. Delectus
+                    dignissimos dolorem dolores esse est,
+                    incidunt iusto laboriosam maxime mollitia
+                    necessitatibus officia quos ratione soluta
+                    tempore veritatis! Adipisci dolorum,
+                    odit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Culpa dolorum fugit ipsam
+                    laborum necessitatibus pariatur placeat
+                    reiciendis ullam. Architecto, harum,
+                    temporibus. Ab adipisci alias aliquid
+                    aperiam aspernatur cumque excepturi
+                    explicabo fugiat harum impedit inventore
+                    ipsa itaque labore natus, nostrum officiis
+                    quaerat quasi reprehenderit sed similique
+                    sit soluta suscipit temporibus. Accusamus
+                    accusantium ad asperiores aut commodi,
+                    debitis expedita id ipsam iste magnam nisi
+                    provident quae quam quibusdam rem velit
+                    voluptatem. Assumenda at dolorem eum minus
+                    odio porro qui sint, voluptatem. Delectus
+                    dignissimos dolorem dolores esse est,
+                    incidunt iusto laboriosam maxime mollitia
+                    necessitatibus officia quos ratione soluta
+                    tempore veritatis! Adipisci dolorum,
+                    odit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Culpa dolorum fugit ipsam
+                    laborum necessitatibus pariatur placeat
+                    reiciendis ullam. Architecto, harum,
+                    temporibus. Ab adipisci alias aliquid
+                    aperiam aspernatur cumque excepturi
+                    explicabo fugiat harum impedit inventore
+                    ipsa itaque labore natus, nostrum officiis
+                    quaerat quasi reprehenderit sed similique
+                    sit soluta suscipit temporibus. Accusamus
+                    accusantium ad asperiores aut commodi,
+                    debitis expedita id ipsam iste magnam nisi
+                    provident quae quam quibusdam rem velit
+                    voluptatem. Assumenda at dolorem eum minus
+                    odio porro qui sint, voluptatem. Delectus
+                    dignissimos dolorem dolores esse est,
+                    incidunt iusto laboriosam maxime mollitia
+                    necessitatibus officia quos ratione soluta
+                    tempore veritatis! Adipisci dolorum,
+                    odit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Culpa dolorum fugit ipsam
+                    laborum necessitatibus pariatur placeat
+                    reiciendis ullam. Architecto, harum,
+                    temporibus. Ab adipisci alias aliquid
+                    aperiam aspernatur cumque excepturi
+                    explicabo fugiat harum impedit inventore
+                    ipsa itaque labore natus, nostrum officiis
+                    quaerat quasi reprehenderit sed similique
+                    sit soluta suscipit temporibus. Accusamus
+                    accusantium ad asperiores aut commodi,
+                    debitis expedita id ipsam iste magnam nisi
+                    provident quae quam quibusdam rem velit
+                    voluptatem. Assumenda at dolorem eum minus
+                    odio porro qui sint, voluptatem. Delectus
+                    dignissimos dolorem dolores esse est,
+                    incidunt iusto laboriosam maxime mollitia
+                    necessitatibus officia quos ratione soluta
+                    tempore veritatis! Adipisci dolorum,
+                    odit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Culpa dolorum fugit ipsam
+                    laborum necessitatibus pariatur placeat
+                    reiciendis ullam. Architecto, harum,
+                    temporibus. Ab adipisci alias aliquid
+                    aperiam aspernatur cumque excepturi
+                    explicabo fugiat harum impedit inventore
+                    ipsa itaque labore natus, nostrum officiis
+                    quaerat quasi reprehenderit sed similique
+                    sit soluta suscipit temporibus. Accusamus
+                    accusantium ad asperiores aut commodi,
+                    debitis expedita id ipsam iste magnam nisi
+                    provident quae quam quibusdam rem velit
+                    voluptatem. Assumenda at dolorem eum minus
+                    odio porro qui sint, voluptatem. Delectus
+                    dignissimos dolorem dolores esse est,
+                    incidunt iusto laboriosam maxime mollitia
+                    necessitatibus officia quos ratione soluta
+                    tempore veritatis! Adipisci dolorum,
+                    odit.</p>
         </>
     );
 }
