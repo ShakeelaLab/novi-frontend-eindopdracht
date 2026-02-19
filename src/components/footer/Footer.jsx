413cba4357154d7f194912ccad8e7fd656319153
ShakeelaLab <shakeela@live.nl>
2026-02-19 17:41:05 +0100

    footer icons darktheme bijgewerkt

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,18 @@
 import './Footer.css';
-import envelope from "../../assets/svg/envelope-simple.svg";
-import facebooklogo from "../../assets/svg/facebook-logo.svg";
-import instagramlogo from "../../assets/svg/instagram-logo.svg";
+import {InstagramLogo, FacebookLogo, EnvelopeSimple,} from "phosphor-react";
 
 function Footer() {
     return (
         <>
             <div className="footer-icons">
                 <a href="https://www.facebook.com/" target="_blank">
-                    <img src={facebooklogo} alt="logo of facebook" />
+                    <FacebookLogo size={32} color="var(--icon-footer)" alt="logo of facebook"/>
                 </a>
                 <a href="https://www.instagram.com/" target="_blank">
-                    <img src={instagramlogo} alt="logo of instagram" />
+                    <InstagramLogo size={32} color="var(--icon-footer)" alt="logo of instagram"/>
                 </a>
                 <a href="mailto:info@jouwdomein.nl" target="_blank">
-                    <img src={envelope} alt="image of an envelope" />
+                    <EnvelopeSimple size={32} color="var(--icon-footer)" alt="logo of envelope"/>
                 </a>
             </div>
             <footer>&copy; Shakeela Jhagroe 2026 for Novi Hogeschool</footer>
